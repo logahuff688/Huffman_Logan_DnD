56a51ef6426c8aa0e020a9a66f771ca63ca2d2c3
logahuff688 <113370990+logahuff688@users.noreply.github.com>
2023-01-30 16:25:47 -0500

    Update main.js

--- script/main.js
+++ script/main.js
@@ -5,7 +5,8 @@
 	// variables always go at the top
 	// these are the connections that you're making to elements on the page
 	// using CSS to select with JavaScript
-	let theButton = document.querySelector("#ButtonOne")
+	let theButton = document.querySelector("#ButtonOne"),
+	theHeading = document.querySelector("#headLine h1")
 
 	// step 3
 	// functionality always goes in the middle -> how do we want the app to behave?
